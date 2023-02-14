2072f65b333da2d7100cbc118132b2f31de61ae6
Kieran Murphy <122936273+ELmonkeman@users.noreply.github.com>
2023-02-14 17:00:35 -0500

    Update Part1.c

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -12,7 +12,7 @@
 
 char LED_Color = 0x01;                       // Global Variable to determine which LED should be blinking
 
-void gpioInit()
+void gpioInit();
 
 
 int main(void)
@@ -35,10 +35,10 @@ int main(void)
 
     while(1)
     {
-        if (LEDColor)
+        if (LED_Color)
             P1OUT ^= BIT0;                  // P1.0 = toggle
         else
-            P6OUT &= ~BIT6;                 // Set P1.0 to 0
+            P6OUT ^= BIT6;                  // P6.6 = toggle
         __delay_cycles(100000);
     }
 }
@@ -66,7 +66,7 @@ void gpioInit(){
       // Configure Button on P2.3 as input with pullup resistor
       P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
       P2REN |= BIT3;                          // P2.3 pull-up register enable
-      P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+      P2IES |= BIT3;                          // P2.3 High --> Low edge
       P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 }
@@ -78,18 +78,17 @@ void gpioInit(){
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    P2IFG &= ~BIT3;                         // Clear P1.3 IFG
+    P2IFG &= ~BIT3;                         // Clear P2.3 IFG
 
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
+    if (P2IES & BIT3) // Check if the interrupt was triggered off a rising edge.
     {
         LED_Color = 0;
-        // @TODO Add code to change which edge the interrupt should be looking for next
+        P2IES &= ~BIT3; // Change edge to look for next to falling
     }
 
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
+    else // Interrupt was triggered off a falling edge.
     {
         LED_Color = 1;
-        // @TODO Add code to change which edge the interrupt should be looking for next
+        P2IES |= BIT3; // Change edge to look for next to rising
     }
 }
-
