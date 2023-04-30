e2a33b5ee83b07bdbfd225602d7b851a58d6a26c
Kieran Murphy <122936273+ELmonkeman@users.noreply.github.com>
2023-04-30 10:10:02 -0400

    Update Part1.c

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -2,7 +2,7 @@
  * Part1.c
  *
  *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *      Author: Kieran Murphy
  *
  *  This code is a template which will change the color of the LED being blinked using the interrupt routine.
  */
@@ -13,22 +13,13 @@
 char LED_Color = 0x01;                       // Global Variable to determine which LED should be blinking
 
 void gpioInit();
-
-
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
 
     gpioInit();
-
-
-
-    // Disable the GPIO power-on default high-impedance mode
-    // to activate previously configured port settings
+   
     PM5CTL0 &= ~LOCKLPM5;
-
-
-
     P2IFG &= ~BIT3;                         // P2.3 IFG cleared
 
     __bis_SR_register(GIE);                 // Enter LPM3 w/interrupt
@@ -38,12 +29,10 @@ int main(void)
         if (LED_Color)
             P1OUT ^= BIT0;                  // P1.0 = toggle
         else
-            P6OUT ^= BIT6;                  // P6.6 = toggle
+            P6OUT ^= BIT6;                 // P6.6 = toggle
         __delay_cycles(100000);
     }
 }
-
-
 /*
  * gpioInit()
  *
@@ -55,13 +44,12 @@ int main(void)
 
 void gpioInit(){
       // Configure RED LED on P1.0 as Output
-      P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+            P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
       P1DIR |= BIT0;                          // Set P1.0 to output direction
 
       // Configure Green LED on P6.6 as Output
       P6OUT &= ~BIT6;                         // Clear P6.6 output latch for a defined power-on state
       P6DIR |= BIT6;                          // Set P6.6 to output direction
-
 
       // Configure Button on P2.3 as input with pullup resistor
       P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
@@ -70,25 +58,21 @@ void gpioInit(){
       P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 }
-
-
-
-
 // Port 2 interrupt service routine
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-    P2IFG &= ~BIT3;                         // Clear P2.3 IFG
-
-    if (P2IES & BIT3) // Check if the interrupt was triggered off a rising edge.
-    {
+    P2IFG &= ~BIT3;                         
+    if (P2IES & BIT3)       
         LED_Color = 0;
-        P2IES &= ~BIT3; // Change edge to look for next to falling
+        P1OUT &= BIT0;
+        P2IES &= BIT3;
     }
 
-    else // Interrupt was triggered off a falling edge.
+    else if (!(P2IES & BIT3))
     {
         LED_Color = 1;
-        P2IES |= BIT3; // Change edge to look for next to rising
+        P6OUT &= ~BIT6;
+        P2IES |= BIT3;
     }
 }
